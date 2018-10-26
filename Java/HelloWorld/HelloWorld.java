5b72d60bf34df43d22a050297500b78877deba32
iamharsha97 <44509861+iamharsha97@users.noreply.github.com>
2018-10-27 01:15:02 +0530

    Updated the comments

--- Java/HelloWorld/HelloWorld.java
+++ Java/HelloWorld/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        
+        //the below line prints the "Hello World" in the console
         System.out.println("Hello World");
     }
 }
